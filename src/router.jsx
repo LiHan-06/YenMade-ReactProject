d1965242cb61a03bd4ce7d9bb3d99780f91a6071
An <easypreference@gmail.com>
2026-02-25 23:44:49 +0800

    fix:二次修正路徑

--- src/router.jsx
+++ src/router.jsx
@@ -18,69 +18,74 @@ import OrderReview from "./OrderReview";
 import OrderSuccess from "./OrderSuccess";
 import CartOrderForm from "./CartOrderForm";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      element: <HomeLayout />,
+      children: [
+        {
+          index: true,
+          element: <App />,
+        },
+      ],
+    },
+    {
+      element: <DefaultLayout />,
+      children: [
+        {
+          path: "AllProducts",
+          element: <AllProducts />,
+        },
+        {
+          path: "product/:id",
+          element: <AProduct />,
+        },
+        {
+          path: "About",
+          element: <About />,
+        },
+        {
+          path: "Blog",
+          element: <Blog />,
+        },
+        {
+          path: "FAQ",
+          element: <FAQ />,
+        },
+        {
+          path: "CheckOut",
+          element: <CheckOut />,
+          children: [
+            {
+              index: true,
+              element: <CartStepOne />,
+            },
+            {
+              path: "CartOrderForm",
+              element: <CartOrderForm />,
+            },
+            {
+              path: "OrderReview",
+              element: <OrderReview />,
+            },
+            {
+              path: "OrderSuccess",
+              element: <OrderSuccess />,
+            },
+          ],
+        },
+        {
+          path: "SignIn",
+          element: <SignIn />,
+        },
+        {
+          path: "SignUp",
+          element: <SignUp />,
+        },
+      ],
+    },
+  ],
   {
-    element: <HomeLayout />,
-    children: [
-      {
-        index: true,
-        element: <App />,
-      },
-    ],
+    basename: "/YenMade-ReactProject",
   },
-  {
-    element: <DefaultLayout />,
-    children: [
-      {
-        path: "AllProducts",
-        element: <AllProducts />,
-      },
-      {
-        path: "/product/:id",
-        element: <AProduct />,
-      },
-      {
-        path: "About",
-        element: <About />,
-      },
-      {
-        path: "Blog",
-        element: <Blog />,
-      },
-      {
-        path: "FAQ",
-        element: <FAQ />,
-      },
-      {
-        path: "CheckOut",
-        element: <CheckOut />,
-        children: [
-          {
-            index: true,
-            element: <CartStepOne />,
-          },
-          {
-            path: "CartOrderForm",
-            element: <CartOrderForm />,
-          },
-          {
-            path: "OrderReview",
-            element: <OrderReview />,
-          },
-          {
-            path: "OrderSuccess",
-            element: <OrderSuccess />,
-          },
-        ],
-      },
-      {
-        path: "SignIn",
-        element: <SignIn />,
-      },
-      {
-        path: "SignUp",
-        element: <SignUp />,
-      },
-    ],
-  },
-]);
+);
